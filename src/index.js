2bc288305e4c41ee749ee3065afcb7879b22df80
Gregorios07 <leonardgregorios@gmail.com>
2024-07-07 23:05:02 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route index element={<Homepage />} />
+          <Route path='index' element={<Homepage />} />
           <Route path='about' element={<About />} />
           <Route path='projects' element={<Projects />} />
           <Route path='contact' element={<Contact />} />
